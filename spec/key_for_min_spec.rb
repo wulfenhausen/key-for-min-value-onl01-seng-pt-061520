1c4ab53cbdd78f2cddb0e8fda896064fed97688a
William Han Findiesen <william.findiesen@gmail.com>
2020-06-28 23:21:09 +0000

    Done.

--- spec/key_for_min_spec.rb
+++ spec/key_for_min_spec.rb
@@ -1,3 +1,4 @@
+=begin
 describe "smallest hash value" do
 
   it "does not call the `#keys` method" do
@@ -61,3 +62,25 @@ describe "smallest hash value" do
   end
 
 end
+=end
+
+## Solution
+
+def key_for_min_value(name_hash)
+  lowest_value = 0
+  lowest_key = nil
+  
+  name_hash.each do |key,value|
+    #if the lowest_value is in it's intial state  
+    #or if the value of the current key is less than the lowest_value
+    #then: set the lowest_value to value and lowest_key to key
+    if lowest_value == 0 || value < lowest_value
+      lowest_value = value 
+      lowest_key = key
+    end
+  end
+  lowest_key
+end
+
+ikea = {:chair => 25, :table => 85, :mattress => 450}
+key_for_min_value(ikea)
